f5afe9e5722042f39a7a4dec88fc614ba9c9bd91
Hon Wi Cong - Student <102761891@students.swinburne.edu.my>
2024-01-09 14:31:50 +0800

    Revert "Initialized Database"
    This reverts commit fb81891f33a789a2ca68a1986239251ae8cf91f9.

--- src/main/java/com/jhipster/app/domain/enumeration/TaxStatus.java
+++
@@ -1,9 +0,0 @@
-package com.jhipster.app.domain.enumeration;
-
-/**
- * The TaxStatus enumeration.
- */
-public enum TaxStatus {
-    Y,
-    N,
-}
--- src/main/webapp/app/shared/model/enumerations/tax-status.model.ts
+++
@@ -1,5 +0,0 @@
-export enum TaxStatus {
-  Y = 'Y',
-
-  N = 'N',
-}
